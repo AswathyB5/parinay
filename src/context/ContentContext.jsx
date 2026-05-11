0aa2a1655b1fb6307a16e1ad69815a43c09ac74f
AswathyB5 <aswathyb.official@gmail.com>
2026-05-11 14:18:48 +0530

    test4

--- src/context/ContentContext.jsx
+++ src/context/ContentContext.jsx
@@ -148,7 +148,7 @@ const initialContent = {
             { id: 2, video: "uploads/12874721_1920_1080_30fps.mp4" }
         ],
         heroImages: [
-            { id: 1, image: "https://img.freepik.com/premium-photo/beautiful-wedding-husband-wife-lovers-man-woman-bride-groom-newlyweds-couple-love-looks-one-one_210028-77.jpg", alt: "Wedding Celebration 1" },
+
             { id: 2, image: "https://img.freepik.com/free-photo/beautiful-wedding-couple-hugging-park_1153-5209.jpg?semt=ais_user_personalization&w=740&q=80", alt: "Wedding Celebration 2" },
             { id: 3, image: "https://i.pinimg.com/736x/d9/16/2a/d9162aded7c5c2347216669d559b265b.jpg", alt: "Wedding Celebration 3" },
             { id: 4, image: "https://img.freepik.com/premium-photo/bride-groom-pose-front-window-dark-room_444642-4894.jpg", alt: "Wedding Celebration 4" }
